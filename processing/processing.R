097e2d512b13c070571ec8d6737a3d9808104106
Isaac Arroyo <68873310+isaacarroyov@users.noreply.github.com>
2026-07-10 18:11:13 +0400

    Updated data - 20260710

--- processing/processing.R
+++ processing/processing.R
@@ -28,7 +28,7 @@ path2repo <- here::here()
 path2input_data <- here::here("input_data")
 path2output_data <- here::here("output_data")
 
-date_last_update <- as.Date("2026-07-03")
+date_last_update <- as.Date("2026-07-10")
 text_file_last_update <- format(x = date_last_update, format = "%e_%B_%Y") %>% str_squish()
 
 # Download and save data
